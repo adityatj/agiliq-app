52154ab95a01f4c93c05ab81b1e1e990d3efeb03
TJ Aditya <tj.aditya@yahoo.com>
2014-03-17 01:49:11 +0530

    update

--- agiliq-app.py
+++ agiliq-app.py
@@ -1,18 +1,31 @@
-import urllib2, urllib, json
+import urllib2, urllib, json,requests
 
 get_data = {}
 
 get_data['client_id'] = 'szZwr7btCFclzzMZJI07zzevkvdPj27Eumzrc5KT3hOrzPZuqp'
 get_data['redirect_uri'] = 'http://localhost/callback'
 get_data['client_secret'] = 'xv8QwDHEZjb9b5lKhZ0teSCauaifbXFeY3Yi9USj1OfzCGCBo6'
-get_data['code'] = 'r3VXJuBydK2Fso9VberHshO6Jp7yn4ypZmrXYmNVDDOKwmv7yO' #herpderp
+get_data['code'] = 'r3VXJuBydK2Fso9VberHshO6Jp7yn4ypZmrXYmNVDDOKwmv7yO'
 
 payload = urllib.urlencode(get_data)
-print payload
 
-req = urllib2.Request('http://join.agiliq.com/oauth/access_token/?' + payload)
-res = urllib2.urlopen(req)
 
-json_data = json.loads(res.readline().strip())
+r = requests.get('http://join.agiliq.com/oauth/access_token/?' + payload)
 
-print json_data['access_token']
+
+access_token = r.json()['access_token']
+
+
+post_data = {}
+
+post_data['access_token'] = access_token
+post_data['first_name'] = 'Aditya'
+post_data['last_name'] = 'TJ'
+post_data['projects_url'] = 'https://github.com/adityatj'
+post_data['code_url'] = 'https://github.com/adityatj/agiliq-app'
+files = {'resume': open('Resume.docx', 'rb')}
+
+r = requests.post('http://join.agiliq.com/api/resume/upload/?', files=files, data=post_data)
+
+print r.status_code
+
